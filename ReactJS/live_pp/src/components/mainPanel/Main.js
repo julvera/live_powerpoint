222fe704717b3a6dd3b9554c1fee475278f6f3dc
JulienVera <julien.vera@hotmail.fr>
2018-10-17 16:40:33 +0200

    initiate Main and App

--- ReactJS/live_pp/src/components/mainPanel/Main.js
+++ ReactJS/live_pp/src/components/mainPanel/Main.js
@@ -1,1 +1,21 @@
-//
+import React from 'react';
+import './main.css';
+
+export default class Main extends React.Component{
+	constructor(props) {
+		super(props);
+		this.state = {
+		}
+	}
+	render() {
+		return (
+			<div className='container-fluid height-100'>VOUAGDHGOANDGKDNGPANG <div className="row height-100">
+			<div className='col-md-3 col-lg-3 height-100 vertical-scroll'>COUCOU </div>
+			<div className='col-md-6 col-lg-6 height-100'> C'est thierry
+			</div>
+			<div className='col-md-3 col-lg-3 height-100'>JEAN JACQUES GOLDMAN
+			</div> </div>
+			</div>
+		);
+	}
+}
