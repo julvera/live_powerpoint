6f84abd58cb1703fb0f63bb79442cb52d2f68a24
JulienVera <julien.vera@hotmail.fr>
2018-10-26 16:04:25 +0200

    check point 6 Node

--- NodeJS/app/routes/content.route.js
+++ NodeJS/app/routes/content.route.js
@@ -9,18 +9,17 @@ const multerMiddleware = multer({ "dest": "/tmp/" });
 
 
 router.param('contentId', function(req, res, next, id) {
+    console.log("router.param");
     req.contentId = id;
     next();
 });
 
 router.route("/contents")
     .get(contentController.list) //list metadata of slide's content
-    .post(multerMiddleware.single("file"), contentController.create); //creates new content from form
+    .post(multerMiddleware.single("file"), contentController.create); //creates new given content
 
-router.route('/content/:contentId')
-    .get(contentController.read)
-    .put(contentController.update)
-    .delete(contentController.delete);
+router.route('/contents/:contentId')
+    .get(contentController.read);
 
 
 module.exports = router;
--- NodeJS/app/routes/presentation.route.js
+++ NodeJS/app/routes/presentation.route.js
@@ -1,63 +1,23 @@
 "use strict";
 
-let express = require("express");
-let fs = require("fs");
-let path = require("path");
+const express = require("express");
+const PresentationUtils = require("../utils/presentation_utils");
 
-let router = express.Router();
+const router = express.Router();
 module.exports = router;
 
-let presDir = JSON.parse(process.env.CONFIG).presentationDirectory;
+const presDir = JSON.parse(process.env.CONFIG).presentationDirectory;
 
 
 router.route("/loadPres")
 	.get(function(req, res) {
-		let map = {};
-		fs.readdir(presDir, function (err, files){
-			if (err) {
-				console.error(err);
-				return res.status(500).end(err.message);
-			}
-			let listPres = [];
-			for (let i = 0; i < files.length; i++) {
-				if (path.extname(files[i]) === ".json") {
-					listPres.push(files[i]);
-				}
-			}
-
-			listPres.forEach(function(fileName){
-				fs.readFile(path.join(presDir, fileName), function(err, data) {
-					if (err) {return console.log(err);}
-
-					let jsonObject = JSON.parse(data);
-					map[jsonObject.id] = jsonObject;
-					if (listPres.length === Object.keys(map).length){
-						res.json(map);
-						res.end();
-					}
-				})
-			})
-		})
-	}
-);
+        console.log("GET /loadPres");
+        PresentationUtils.listDirFiles(presDir, res);
+	});
 
 router.route("/savePres")
 	.post(function(req, res) {
-		let data = "";
-		req.on("data", function (chunk) {
-            data += chunk.toString();
-        });
-
-		req.on("end", () => {
-            let jsonObject = JSON.parse(data);
-            let filePath = path.join(presDir, jsonObject.id + ".pres.json");
-
-            fs.writeFile(filePath, JSON.stringify(jsonObject), "utf8", function (err) {
-                if (err) {console.log("shit Happened");}
-
-                res.end("File successfully saved!");
-            });
-		});
-
+        console.log("POST /savePres");
+        PresentationUtils.saveNewPres(presDir, req, res);
 	}
 );
