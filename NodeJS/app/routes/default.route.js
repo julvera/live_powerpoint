ceedca1f71746f847501a77f3a4a7b06214365e1
JulienVera <julien.vera@hotmail.fr>
2018-11-08 18:02:01 +0100

    add login endpoint that calls JEE webservice and redirects to watch/ or admin/

--- NodeJS/app/routes/default.route.js
+++ NodeJS/app/routes/default.route.js
@@ -1,37 +1,63 @@
 "use strict";
 
 const express = require("express");
+const http = require("http");
 const router = express.Router();
 
 module.exports = router;
+
 
 router.route("/")
     .get(function(req, res) {
         res.send("It Works!");
     });
 
-// router.post("/login", function(request, response) {
-//     console.log("/login " + request.body["login"]);
-//     let data = JSON.stringify({
-//         "login": request.body["login"],
-//         "password": request.body["password"]
-//     });
-//     let options = {
-//         method: "POST",
-//         host: "localhost",
-//         port: "8000",
-//         path: "TODO",
-//         headers: {
-//             "Content-Type": "application/json; charset=utf-8",
-//             "Content-Length": data.length
-//         }
-//     };
-//
-//     if (request.body["login"] === "jul") {
-//         console.log("ADMIN ");
-//         response.send({"role": "ADMIN"});
-//     }
-//     else {
-//         response.send({"role": "USER"});
-//     }
-// });
+router.route("/login")
+    .post("/login", function(request, response) {
+        console.log("/login " + request.body["login"]);
+        let data = JSON.stringify({
+            "login": request.body["login"],
+            "password": request.body["password"]
+        });
+        let options = {
+            method: "POST",
+            host: "localhost",
+            port: "8080",
+            path: "/FrontAuthWatcherWebService/rest/WatcherAuth",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                "Content-Length": data.length
+            }
+        };
+
+        let req = http.request(options, function(res) {
+            let msg = "";
+            res.setEncoding("utf8");
+
+            res.on("data", function(chunk) {
+                msg += chunk;
+            });
+
+            res.on("end", function() {
+                console.log(msg);
+                if(msg === "")
+                {
+                    console.log("Empty reply from JEE webservice");
+                    //response.redirect("/TODO: page d'erreur?");
+                    response.send(msg);
+                } else {
+                    let reply = JSON.parse(msg);
+                    if(reply.role === "admin"){
+                        //response.redirect("/admin");
+                        response.send(msg);
+                    } else {
+                        //response.redirect("/watch");
+                        response.send(msg);
+                    }
+                }
+            });
+        });
+
+        req.write(data);
+        req.end();
+    });
